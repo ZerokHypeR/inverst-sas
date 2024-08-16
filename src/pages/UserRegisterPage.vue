0618766ea6fb8a7520eae56e831042ddcd8b6da7
ZerokHypeR <zerokhyper1047@gmail.com>
2024-08-16 16:58:12 -0500

    pendiente toca seguir configurando el index y configurar las carpetas para tener mas orden

--- src/pages/UserRegisterPage.vue
+++ src/pages/UserRegisterPage.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1>2</h1>
+  <h1> gh </h1>
 </template>
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,20 +1,24 @@
 import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
+ 
+
   {
     path: '/',
-    name: 'UserLoginPage',
-    component: () => import('../pages/UserLoginPage.vue'),
-  },
-  {
-    path: '/register',
     name: 'UserRegisterPage',
     component: () => import('../pages/UserRegisterPage.vue'),
   },
   {
-    path: '/home',
-    redirect: '/home.html' // Redirige a home.html en la carpeta public
+    path: '/login',
+    redirect: '/login.html' // Redirige a home.html en la carpeta public
   },
+
+  {
+    path: '/Registro',
+    redirect: '/register.html ' // Redirige a home.html en la carpeta public
+  },
+
+
   {
     path: '/dashboard',
     name: 'DashboardPage',
