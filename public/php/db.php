8f0b05adb1c57f0ce0e3e7164a4bf8059a132c6f
ZerokHypeR <zerokhyper1047@gmail.com>
2024-08-20 15:10:51 -0500

    cambio de carpetas y implemento de php base de datos

---
+++ public/php/db.php
@@ -0,0 +1,8 @@
+<?php
+require '../vendor/autoload.php'; // Asegúrate de ajustar la ruta si es necesario
+
+$client = new MongoDB\Client("mongodb://localhost:27017");
+$database = $client->inverst; // Nombre de la base de datos
+$collection = $database->usuarios; // Nombre de la colección
+
+
